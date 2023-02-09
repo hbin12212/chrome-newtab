169392c0ad606587dce7020a9e36933fbd7c91fa
김효빈 <52522662+hbin12212@users.noreply.github.com>
2023-02-09 15:17:47 +0900

    [FEAT] : 북마크 추가 삭제 기능

---
+++ js/bookmark-toggle.js
@@ -0,0 +1,42 @@
+const bookMarkComponent = document.getElementById("bookmark");
+const bookMarkOpenIcon = document.getElementById("bookmark-open");
+const bookMarkCloseIcon = document.getElementById("bookmark-close");
+
+// <--북마크 창-->
+const isBookMarkOpen = localStorage?.getItem("isBookMarkOpen");
+if (isBookMarkOpen === "close") {
+    //localstorage에 isBookMarkOpen이 close라면
+    bookMarkComponent.style.display = "none";
+    bookMarkOpenIcon.style.display = "none";
+    bookMarkCloseIcon.style.display = "flex";
+} else {
+    //localstorage에 isBookMarkOpen이 open이라면
+    bookMarkComponent.style.display = "block";
+    bookMarkOpenIcon.style.display = "flex";
+    bookMarkCloseIcon.style.display = "none";
+}
+
+// <--북마크 창 toggle-->
+const toggle = () => {
+    let isBookMarkOpen = localStorage?.getItem("isBookMarkOpen");
+    if (isBookMarkOpen) {
+        //localstorage에 isBookMarkOpen이 있다면
+        if (isBookMarkOpen === "open") {
+            //닫기
+            localStorage?.setItem("isBookMarkOpen", "close");
+            bookMarkComponent.style.display = "none";
+            bookMarkOpenIcon.style.display = "none";
+            bookMarkCloseIcon.style.display = "flex";
+        } else {
+            //열기
+            localStorage.setItem("isBookMarkOpen", "open");
+            bookMarkComponent.style.display = "block";
+            bookMarkOpenIcon.style.display = "flex";
+            bookMarkCloseIcon.style.display = "none";
+        }
+    } else {
+        //localstorage에 isBookMarkOpen이 없다면
+        localStorage.setItem("isBookMarkOpen", "close");
+        bookMarkComponent.style.display = "none";
+    }
+};
--- js/bookmark.js
+++ js/bookmark.js
@@ -1,69 +1,17 @@
-const bookMarkComponent = document.getElementById("bookmark");
 const newBookMarkInputBox = document.getElementById("new-bookmark-item");
 const bookMarkItemList = document.getElementById("bookmark-list");
 
-// <--북마크 창-->
-const isBookMarkOpen = localStorage?.getItem("isBookMarkOpen");
-if (isBookMarkOpen === "close") {
-    //localstorage에 isBookMarkOpen이 close라면
-    bookMarkComponent.style.display = "none";
+// <--bookMarkList 초기 설정-->
+let bookMarkList = [];
+if (localStorage.getItem("bookMarkList")) {
+    bookMarkList = JSON?.parse(localStorage.getItem("bookMarkList"));
 } else {
-    bookMarkComponent.style.display = "block";
+    localStorage.setItem("bookMarkList", bookMarkList);
 }
 
-// <--북마크 창 toggle-->
-const toggle = () => {
-    let isBookMarkOpen = localStorage?.getItem("isBookMarkOpen");
-    if (isBookMarkOpen) {
-        //localstorage에 isBookMarkOpen이 있다면
-        if (isBookMarkOpen === "open") {
-            //닫기
-            localStorage?.setItem("isBookMarkOpen", "close");
-            bookMarkComponent.style.display = "none";
-        } else {
-            //열기
-            localStorage.setItem("isBookMarkOpen", "open");
-            bookMarkComponent.style.display = "block";
-        }
-    } else {
-        //localstorage에 isBookMarkOpen이 없다면
-        localStorage.setItem("isBookMarkOpen", "close");
-        bookMarkComponent.style.display = "none";
-    }
-};
-
-// <--북마크 추가 toggle-->
-let addBtnClick = false;
-newBookMarkInputBox.style.display = "none";
-
-const newBookMarkToggle = () => {
-    addBtnClick = !addBtnClick;
-    if (addBtnClick) {
-        newBookMarkInputBox.style.display = "block";
-    } else {
-        newBookMarkInputBox.style.display = "none";
-    }
-};
-
-// <--북마크 저장-->
-let bookMarkList = localStorage?.getItem("bookMarkList");
-const saveItem = () => {
-    let urlName = document.getElementById("new-url-name").value;
-    let url = document.getElementById("new-url").value;
-    bookMarkList.unshift({ name: urlName, url: url });
-    localStorage.setItem("bookMarkList", JSON.stringify(bookMarkList));
-    document.getElementById("new-url").value = "";
-    newBookMarkToggle();
-    showBookMarkList();
-};
-
-// <--북마크 삭제-->
-const deleteItem = () => {};
-
-// <--북마크 리스트-->
+// <--북마크 리스트 표시-->
 const showBookMarkList = () => {
     let template = "";
-    console.log(bookMarkList);
     bookMarkList?.map((it, idx) => {
         template += `<div class="bookmark-item" id="bookmark-item${idx}">
             <div class="item-header">
@@ -79,6 +27,48 @@ const showBookMarkList = () => {
         </div>`;
     });
     bookMarkItemList.insertAdjacentHTML("afterbegin", template);
+};
+
+// <--북마크 추가 버튼-->
+let addBtnClick = false;
+newBookMarkInputBox.style.display = "none";
+
+const newBookMarkToggle = () => {
+    addBtnClick = !addBtnClick;
+    if (addBtnClick) {
+        newBookMarkInputBox.style.display = "block";
+    } else {
+        newBookMarkInputBox.style.display = "none";
+    }
+};
+
+// <--북마크 저장-->
+const saveItem = () => {
+    let urlName = document.getElementById("new-url-name").value;
+    let url = document.getElementById("new-url").value;
+    bookMarkList.unshift({ name: urlName, url: url });
+    localStorage.setItem("bookMarkList", JSON.stringify(bookMarkList));
+    document.getElementById("new-url-name").value = "";
+    document.getElementById("new-url").value = "";
+    newBookMarkToggle();
+    while (bookMarkItemList.firstChild) {
+        bookMarkItemList.removeChild(bookMarkItemList.firstChild);
+    }
+    showBookMarkList();
+};
+
+// <--북마크 삭제-->
+const deleteItem = (idx) => {
+    const flag = window.confirm("정말 삭제하시겠습니까?");
+    if (flag) {
+        bookMarkList?.splice(idx, 1);
+        localStorage.setItem("bookMarkList", JSON.stringify(bookMarkList));
+        while (bookMarkItemList.firstChild) {
+            bookMarkItemList.removeChild(bookMarkItemList.firstChild);
+        }
+        showBookMarkList();
+    }
+    return;
 };
 
 showBookMarkList();
