53bd5edb0bac4cdd6b8d3f81aaa9ba6b46680236
HYOBIN <sg05138@naver.com>
2023-04-14 10:01:27 +0900

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -1,36 +1,36 @@
 (function () {
-    const timeElement = document.getElementById("time");
     const dateElement = document.getElementById("date");
-
+    const timeElement = document.getElementById("time");
+    
     const modifyNumber = (number) => {
-        return parseInt(number) < 10 ? "0" + number : number;
+         return parseInt(number) < 10 ? "0" + number: number;
     };
-
-    const getDate = () => {
-        const nowDate = new Date();
-        const week = ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"];
-        let month = modifyNumber(nowDate.getMonth() + 1);
-        let date = modifyNumber(nowDate.getDate());
-        let day = week[nowDate.getDay()];
-        setDate(month, date, day);
+    
+    const getNowDate = () => {
+            const nowDate = new Date();
+            const week = ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"];
+            let month = modifyNumber(nowDate.getMonth() + 1);
+            let date = modifyNumber(nowDate.getDate());
+            let day = week[nowDate.getDay()];
+            setNowDate(month, date, day);
     };
-
-    const setDate = (month, date, day) => {
+    
+    const setNowDate = (month, date, day) => {
         dateElement.textContent = `${month}월${date}일${day}`;
     };
-
-    const getTime = () => {
+    
+    const getNowTime = () => {
         const nowDate = new Date();
         let hour = modifyNumber(nowDate.getHours());
         let minute = modifyNumber(nowDate.getMinutes());
-        setTime(hour, minute);
+        setNowTime(hour, minute);
     };
-
-    const setTime = (hour, minute) => {
-        timeElement.textContent = `${hour}:${minute}`;
+    
+    const setNowTime = (hour, minute) => {
+            timeElement.textContent = `${hour}:${minute}`;
     };
-
-    getDate();
-    getTime();
-    setInterval(getTime, 1000);
+    
+    getNowDate();
+    getNowTime();
+    setInterval(getNowTime, 1000);
 })();
