43baf09242bf3c2ca8f8938945ab5b9ab9fa70fd
HYOBIN <hbin12212@gmail.com>
2024-05-31 12:55:13 +0900

    [FIX] : add space

--- src/js/clock.js
+++ src/js/clock.js
@@ -1,35 +1,35 @@
 (function () {
-    const dateElement = document.getElementById("date");
-    const timeElement = document.getElementById("time");
-    
+    const dateElement = document.getElementById('date');
+    const timeElement = document.getElementById('time');
+
     const modifyNumber = (number) => {
-         return parseInt(number) < 10 ? "0" + number: number;
+        return parseInt(number) < 10 ? '0' + number : number;
     };
-    
+
     const getNowDate = () => {
-            const nowDate = new Date();
-            const week = ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"];
-            let month = modifyNumber(nowDate.getMonth() + 1);
-            let date = modifyNumber(nowDate.getDate());
-            let day = week[nowDate.getDay()];
-            setNowDate(month, date, day);
+        const nowDate = new Date();
+        const week = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
+        let month = modifyNumber(nowDate.getMonth() + 1);
+        let date = modifyNumber(nowDate.getDate());
+        let day = week[nowDate.getDay()];
+        setNowDate(month, date, day);
     };
-    
+
     const setNowDate = (month, date, day) => {
-        dateElement.textContent = `${month}월${date}일 ${day}`;
+        dateElement.textContent = `${month}월 ${date}일 ${day}`;
     };
-    
+
     const getNowTime = () => {
         const nowDate = new Date();
         let hour = modifyNumber(nowDate.getHours());
         let minute = modifyNumber(nowDate.getMinutes());
         setNowTime(hour, minute);
     };
-    
+
     const setNowTime = (hour, minute) => {
-            timeElement.textContent = `${hour}:${minute}`;
+        timeElement.textContent = `${hour}:${minute}`;
     };
-    
+
     getNowDate();
     getNowTime();
     setInterval(getNowTime, 1000);
